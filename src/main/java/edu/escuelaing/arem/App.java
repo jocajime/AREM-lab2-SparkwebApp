24abd8a011ef7021df85a99a07d11f0e48dd9b0d
joel carvajal <joel.carvajal@mail.escuelaing.edu.co>
2021-02-05 11:35:14 -0500

    app base, procfile, pom build

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -1,13 +1,19 @@
 package edu.escuelaing.arem;
-
+import static spark.Spark.*;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 }
